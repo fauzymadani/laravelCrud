712a67d5a4a5e84df6c345b635745cc6b416c319
fauzymadani <keperluansekolahfauzy@gmail.com>
2024-10-22 12:46:22 +0700

    update dependencies

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,7 +47,7 @@
 
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Sisfo pegawai</h1>
+                        <h1 class="h3 mb-0 text-gray-800">Sisfo pegawai romusha fauzy</h1>
 
                     </div>
                     @yield('content')
