08de9be1d19897a6a37d38ae91823de333c0a9da
hauraa2024 <haurafadilla16@gmail.com>
2024-11-07 07:49:06 +0700

    fix login buton not working

--- resources/views/sesi/index.blade.php
+++ resources/views/sesi/index.blade.php
@@ -1,72 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
-    <title>Sisfo kepegawaian</title>
+    <title>sisfo</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <!-- Custom styles for this template-->
     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
-
+    
 </head>
 
-<body class="bg-gradient-primary">
+<body>
 
-    <div class="container">
+  <div class="container">
 
         <!-- Outer Row -->
-        <div class="row justify-content-center">
-
+        <div class="row justify-content-center align-items-center" style="height: 100vh;"> 
             <div class="col-xl-10 col-lg-12 col-md-9">
-
-                <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card o-hidden border-0 shadow-lg my-5 login-card">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            
+                            <div class="col-lg-6 d-none d-lg-block text-center">
+                                {{-- <img src="https://i.pinimg.com/originals/0d/45/cc/0d45cce4c9397bb2c957d48c3759038a.gif" alt="Profile Image" class="profile-image"> --}}
+                            </div>
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">SElamat datang</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Selamat Datang</h1>
                                     </div>
                                     <form class="user" action="/sesi/login" method="POST">
                                         @csrf
                                         <div class="form-group">
-                                            <input type="email" class="form-control form-control-user"
-                                                name="name" value="{{Session::get('email')}}"
-                                                placeholder="Masukkan Alamat Email Anda">
+                                            <input type="email" class="form-control form-control-user" name="email" value="{{Session::get('email')}}" placeholder="Masukan Alamat Email" required>
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" name="password" class="form-control form-control-user"
-                                                 placeholder="Masukkan Password Anda">
+                                            <input type="password" name="password" class="form-control form-control-user" placeholder="Masukan Password Anda" required>
                                         </div>
-                                        
-                                        <button type="submit" class="btn btn-primary btn-user btn-block">Login</button>
-                                        <hr>
-                                        
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">LOGIN</button>
                                     </form>
                                     <hr>
-                                    
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </div>
-
         </div>
 
     </div>
@@ -74,16 +60,10 @@
     <!-- Bootstrap core JavaScript-->
     <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-
     <!-- Core plugin JavaScript-->
     <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
-
     <!-- Custom scripts for all pages-->
     <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
 
-    <!-- Page level plugins -->
-    {{-- <script src="{{ asset('vendor/chart.js/Chart.min.js') }}"></script> --}}
-
 </body>
-
 </html>
