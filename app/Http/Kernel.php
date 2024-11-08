d376f50d704f648039895772a488a870fd6be877
hauraa2024 <haurafadilla16@gmail.com>
2024-11-08 13:17:25 +0700

    add middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,8 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        'iniLogin' => \App\Http\Middleware\iniLogin::class,
+        'iniTamu' => \App\Http\Middleware\iniTamu::class,
     ];
 }
